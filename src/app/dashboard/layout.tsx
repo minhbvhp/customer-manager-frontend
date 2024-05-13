78e5089b0022e80cb47d0e8da90217bb44e15056
Minh <tranminhbvhp@gmail.com>
2024-05-13 22:24:27 +0700

    FIX (dashboard layout): dont use Layout components any more

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,7 +1,3 @@
-import { Layout } from "antd";
-import { Header, Footer, Content } from "antd/lib/layout/layout";
-import Sider from "antd/lib/layout";
-
 export const metadata = {
   title: "Dashboard",
   description: "Dashboard",
@@ -13,15 +9,10 @@ export default function DashboardLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <>
-      <Layout>
-        <Header>Tiêu đề</Header>
-        <Layout>
-          {/* <Sider>Thanh bên trái</Sider> */}
-          <Content>{children}</Content>
-        </Layout>
-        {/* <Footer>Footer</Footer> */}
-      </Layout>
-    </>
+    <div>
+      <div>Sidenav</div>
+
+      <div>{children}</div>
+    </div>
   );
 }
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,9 +2,5 @@ import styles from "../page.module.css";
 import { Button } from "antd";
 
 export default function Dashboard() {
-  return (
-    <>
-      <Button type="primary">Button</Button>
-    </>
-  );
+  return <>Content here</>;
 }
