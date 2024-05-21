5f426da2e8dc38db43560116e58ca0c5033caa46
trankhoaminh <tranminhbvhp@gmail.com>
2024-05-21 10:31:48 +0700

    FIX (dashboard): menu item not hightlight when refresh FEAT (dashboard): use pathname and router hook to navigation

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,27 +1,10 @@
 "use client";
 import React from "react";
-import { UserOutlined, FundFilled, SettingOutlined } from "@ant-design/icons";
-import { Layout, Menu, Divider, Avatar, Space, Flex, theme } from "antd";
-import type { GetProp, MenuProps } from "antd";
-import Link from "next/link";
+import { FundFilled, SettingOutlined } from "@ant-design/icons";
+import { Layout, Divider, Avatar, Space, theme } from "antd";
+import DashboardMenu from "@/components/Dashboard/Menu";
 
 const { Header, Content, Footer, Sider } = Layout;
-
-type MenuItem = GetProp<MenuProps, "items">[number];
-
-const items: MenuItem[] = [
-  {
-    key: "customer_key_menu_item",
-    label: "Khách hàng",
-    icon: <UserOutlined />,
-    children: [
-      {
-        key: "customer_list_sub_menu_item",
-        label: <Link href="/dashboard/customer/list">Danh sách</Link>,
-      },
-    ],
-  },
-];
 
 export default function DashboardLayout({
   children,
@@ -46,7 +29,7 @@ export default function DashboardLayout({
           />
         </div>
         <Divider style={{ margin: 0 }} />
-        <Menu mode="inline" items={items} />
+        <DashboardMenu />
       </Sider>
 
       <Layout>
---
+++ src/components/Dashboard/Menu.tsx
@@ -0,0 +1,62 @@
+"use client";
+import {
+  UserOutlined,
+  HomeOutlined,
+  FileTextOutlined,
+} from "@ant-design/icons";
+import { Menu } from "antd";
+import type { GetProp, MenuProps } from "antd";
+import { usePathname, useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+
+type MenuItem = GetProp<MenuProps, "items">[number];
+
+const items: MenuItem[] = [
+  {
+    key: "/dashboard",
+    label: "Trang chủ",
+    icon: <HomeOutlined />,
+  },
+  {
+    key: "/dashboard/customer",
+    label: "Khách hàng",
+    icon: <UserOutlined />,
+  },
+  {
+    key: "/dashboard/policy",
+    label: "Đơn bảo hiểm",
+    icon: <FileTextOutlined />,
+  },
+];
+
+const DashboardMenu: React.FC = () => {
+  const pathname = usePathname();
+  const router = useRouter();
+
+  const [current, setCurrent] = useState(
+    pathname === "/" || pathname === "" ? "/dashboard" : pathname
+  );
+
+  function handleClick(e: any) {
+    router.push(e.key);
+  }
+
+  useEffect(() => {
+    if (pathname) {
+      if (current !== pathname) {
+        setCurrent(pathname);
+      }
+    }
+  }, [pathname, current]);
+
+  return (
+    <Menu
+      mode="inline"
+      items={items}
+      selectedKeys={[current]}
+      onClick={handleClick}
+    />
+  );
+};
+
+export default DashboardMenu;
