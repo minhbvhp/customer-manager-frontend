2067f0b214cf57456c7c35c4bc37b06fd99561c0
trankhoaminh <tranminhbvhp@gmail.com>
2024-05-28 16:50:16 +0700

    FEAT (customer table): add filter by searched name

--- src/components/Customers/Table.tsx
+++ src/components/Customers/Table.tsx
@@ -1,10 +1,14 @@
 "use client";
 import { Customer } from "@/app/lib/definitions";
-import { Table } from "antd";
-import type { TableColumnsType } from "antd";
-import { useEffect, useState } from "react";
+import { Button, Input, Space, Table } from "antd";
+import { useEffect, useRef, useState } from "react";
+import type { FilterDropdownProps } from "antd/es/table/interface";
+import type { InputRef, TableColumnsType, TableColumnType } from "antd";
+import { SearchOutlined } from "@ant-design/icons";
+import Highlighter from "react-highlight-words";
+
 interface DataType {
-  key: React.Key;
+  key: string;
   fullName: string;
   taxCode: string;
   address: string;
@@ -16,43 +20,32 @@ export default function CustomerTable({
   customers: Customer[];
 }) {
   const [isLoading, setIsLoading] = useState(false);
+  const [searchText, setSearchText] = useState("");
+  const [searchedColumn, setSearchedColumn] = useState("");
+  const searchInput = useRef<InputRef>(null);
 
   useEffect(() => {
     if (!customers) setIsLoading(true);
   }, [customers]);
 
-  const columns: TableColumnsType<DataType> = [
-    {
-      title: "Khách hàng",
-      dataIndex: "fullName",
-    },
-    {
-      title: "Mã số thuế",
-      dataIndex: "taxCode",
-    },
-    {
-      title: "Địa chỉ",
-      dataIndex: "address",
-      filters: [
-        {
-          text: "Hải Phòng",
-          value: "Hải Phòng",
-        },
-        {
-          text: "Hà Nội",
-          value: "Hà Nội",
-        },
-        {
-          text: "TP. Hồ Chí Minh",
-          value: "TP. Hồ Chí Minh",
-        },
-      ],
-      onFilter: (value, record) =>
-        record.address.indexOf(value as string) !== -1,
-    },
-  ];
+  const handleSearch = (
+    selectedKeys: string[],
+    confirm: FilterDropdownProps["confirm"],
+    dataIndex: DataIndex
+  ) => {
+    confirm();
+    setSearchText(selectedKeys[0]);
+    setSearchedColumn(dataIndex);
+  };
 
-  const data = customers.map((customer: any) => ({
+  const handleReset = (clearFilters: () => void) => {
+    clearFilters();
+    setSearchText("");
+  };
+
+  type DataIndex = keyof DataType;
+
+  const data: DataType[] = customers.map((customer: any) => ({
     key: customer.id,
     fullName: customer.fullName,
     taxCode: customer.taxCode,
@@ -61,11 +54,115 @@ export default function CustomerTable({
       : `${customer.ward.fullName}`,
   }));
 
+  const arrayAddress = customers.map((customer: any) => ({
+    text: customer.ward.district.province.name,
+    value: customer.ward.district.province.name,
+  }));
+
+  const key = "value";
+  const addressFilter = [
+    ...new Map(arrayAddress.map((item) => [item[key], item])).values(),
+  ];
+
+  const getColumnSearchProps = (
+    dataIndex: DataIndex
+  ): TableColumnType<DataType> => ({
+    filterDropdown: ({
+      setSelectedKeys,
+      selectedKeys,
+      confirm,
+      clearFilters,
+    }) => (
+      <div style={{ padding: 8 }} onKeyDown={(e) => e.stopPropagation()}>
+        <Input
+          ref={searchInput}
+          placeholder="Tìm theo tên"
+          value={selectedKeys[0]}
+          onChange={(e) =>
+            setSelectedKeys(e.target.value ? [e.target.value] : [])
+          }
+          onPressEnter={() =>
+            handleSearch(selectedKeys as string[], confirm, dataIndex)
+          }
+          style={{ marginBottom: 8, display: "block" }}
+        />
+        <Space>
+          <Button
+            type="primary"
+            onClick={() =>
+              handleSearch(selectedKeys as string[], confirm, dataIndex)
+            }
+            icon={<SearchOutlined />}
+            size="small"
+            style={{ width: 90 }}
+          >
+            Tìm
+          </Button>
+          <Button
+            onClick={() => clearFilters && handleReset(clearFilters)}
+            size="small"
+            style={{ width: 90 }}
+          >
+            Xóa
+          </Button>
+        </Space>
+      </div>
+    ),
+    filterIcon: (filtered: boolean) => (
+      <SearchOutlined style={{ color: filtered ? "#1677ff" : undefined }} />
+    ),
+    onFilter: (value, record) =>
+      record[dataIndex]
+        .toString()
+        .toLowerCase()
+        .includes((value as string).toLowerCase()),
+    onFilterDropdownOpenChange: (visible) => {
+      if (visible) {
+        setTimeout(() => searchInput.current?.select(), 100);
+      }
+    },
+    render: (text) =>
+      searchedColumn === dataIndex ? (
+        <Highlighter
+          highlightStyle={{ backgroundColor: "#ffc069", padding: 0 }}
+          searchWords={[searchText]}
+          autoEscape
+          textToHighlight={text ? text.toString() : ""}
+        />
+      ) : (
+        text
+      ),
+  });
+
+  const columns: TableColumnsType<DataType> = [
+    {
+      title: "Khách hàng",
+      dataIndex: "fullName",
+      ...getColumnSearchProps("fullName"),
+    },
+    {
+      title: "Mã số thuế",
+      dataIndex: "taxCode",
+      ...getColumnSearchProps("taxCode"),
+    },
+    {
+      title: "Địa chỉ",
+      dataIndex: "address",
+      filters: addressFilter,
+      onFilter: (value, record) =>
+        record.address.indexOf(value as string) !== -1,
+    },
+  ];
+
   return (
     <Table
       loading={isLoading}
       pagination={{ pageSize: 5 }}
-      locale={{ emptyText: "Không tìm thấy khách hàng" }}
+      locale={{
+        emptyText: "Không tìm thấy khách hàng",
+        filterReset: "Xóa",
+        filterConfirm: "Lọc",
+      }}
       columns={columns}
       dataSource={data}
       showSorterTooltip={{ target: "sorter-icon" }}
