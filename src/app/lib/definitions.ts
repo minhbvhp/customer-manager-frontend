fe029eac343e83631a2d22484daa54f191d8f612
Minh <tranminhbvhp@gmail.com>
2024-06-25 21:15:14 +0700

    FEAT (definitions): add Contact type and add to Customer type

--- src/app/lib/definitions.ts
+++ src/app/lib/definitions.ts
@@ -1,9 +1,15 @@
+export type Contact = {
+  name: string;
+  phone: string;
+};
+
 export type Customer = {
   id: string;
   fullName: string;
   taxCode: string;
   urn: string;
   street: string;
+  contacts: Contact[];
   wardCode: string;
   ward: any;
 };
