4eac2554fbcdff40dd1742d3acdaf6af9c0dd2e6
Minh <tranminhbvhp@gmail.com>
2024-07-13 23:10:56 +0700

    FEAT (Middleware): refresh token seems ok

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,27 +1,84 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 import { jwtDecode } from "jwt-decode";
+import { cookies } from "next/headers";
 
 const privatePaths = ["/dashboard"];
 const authPaths = ["/login"];
 
-export function middleware(request: NextRequest) {
+export async function middleware(request: NextRequest) {
   const { pathname } = request.nextUrl;
-  const accessToken = request.cookies?.get("accessToken")?.value;
+  const accessTokenFromCookie = request.cookies?.get("accessToken")?.value;
+  const refreshTokenFromCookie = request.cookies?.get("refreshToken")?.value;
 
-  const decoded = accessToken ? jwtDecode(accessToken) : "";
+  //Check if login require
+  if (privatePaths.some((path) => pathname.startsWith(path))) {
+    if (!accessTokenFromCookie && !refreshTokenFromCookie) {
+      return NextResponse.redirect(new URL("/login", request.url));
+    }
 
-  //Check if need login
-  if (privatePaths.some((path) => pathname.startsWith(path)) && !accessToken) {
-    return NextResponse.redirect(new URL("/login", request.url));
+    if (!accessTokenFromCookie && refreshTokenFromCookie) {
+      try {
+        const url = process.env.BACKEND_URL + "/auth/refresh";
+        const res = await fetch(url, {
+          method: "POST",
+          body: JSON.stringify({
+            refreshToken: refreshTokenFromCookie,
+          }),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
+
+        const parsedRes = await res.json();
+
+        console.log(parsedRes);
+
+        const { jwtToken, refreshToken } = parsedRes;
+
+        const accessTokenDecode = jwtDecode(jwtToken);
+
+        const refreshTokenDecode = jwtDecode(refreshToken);
+
+        const response = NextResponse.next();
+
+        response.cookies.set({
+          name: "accessToken",
+          value: jwtToken,
+          secure: true,
+          httpOnly: true,
+          expires: new Date(accessTokenDecode.exp! * 1000),
+        });
+
+        response.cookies.set({
+          name: "refreshToken",
+          value: refreshToken,
+          secure: true,
+          httpOnly: true,
+          expires: new Date(refreshTokenDecode.exp! * 1000),
+        });
+
+        return response;
+      } catch {
+        return NextResponse.redirect(new URL("/login", request.url));
+      }
+    }
   }
 
   //Redirect dash board if already login
-  if (authPaths.some((path) => pathname.startsWith(path)) && accessToken) {
-    return NextResponse.redirect(new URL("/dashboard", request.url));
-  }
+  if (authPaths.some((path) => pathname.startsWith(path))) {
+    const accessTokenFromCookie = request.cookies?.get("accessToken")?.value;
 
-  return NextResponse.next();
+    const response = NextResponse.next();
+    response.cookies.delete("accessToken");
+    response.cookies.delete("refreshToken");
+
+    if (accessTokenFromCookie) {
+      return NextResponse.redirect(new URL("/dashboard", request.url));
+    }
+
+    return response;
+  }
 }
 
 export const config = {
